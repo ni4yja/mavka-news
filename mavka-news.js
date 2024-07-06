9ef617a0d79631122c80d6b5616848e7a8c55ea4
Liuba Kuibida <k2.liuba@gmail.com>
2024-07-06 15:04:47 +0200

    update with env variables

--- mavka-news.js
+++ mavka-news.js
@@ -1,13 +1,19 @@
 import fs from 'node:fs'
+import process from 'node:process'
+import dotenv from 'dotenv'
 import puppeteer from 'puppeteer'
 import { addArticleToNotionDatabase } from './add-article-to-notion.js'
+
+dotenv.config()
+
+const { PATH_TO_WEBSITE, PATH_TO_LOGFILE } = process.env
 
 async function scrapeNews() {
   const browser = await puppeteer.launch()
   const page = await browser.newPage()
 
   try {
-    await page.goto('https://lb.ua/culture/analytics')
+    await page.goto(PATH_TO_WEBSITE)
 
     const articles = await page.evaluate(() => {
       const articleNodes = document.querySelectorAll('.item-article')
@@ -32,7 +38,7 @@ async function scrapeNews() {
       return articleList
     })
 
-    fs.appendFileSync('/Users/k2/Development/mavka/logfile.log', `${JSON.stringify(articles)}\n`)
+    fs.appendFileSync(PATH_TO_LOGFILE, `${JSON.stringify(articles)}\n`)
 
     for (const article of articles) await addArticleToNotionDatabase(article)
 
@@ -40,7 +46,7 @@ async function scrapeNews() {
   }
   catch (error) {
     console.error(error)
-    fs.appendFileSync('/Users/k2/Development/mavka/logfile.log', `${error.toString()}\n`)
+    fs.appendFileSync(PATH_TO_LOGFILE, `${error.toString()}\n`)
   }
   finally {
     await browser.close()
